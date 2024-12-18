5c85f542cb715b0187dc4c93ac103ccc2c7fc71c
Emerson0102a <w15150055831@163.com>
2024-12-18 14:10:04 +0800

    五子棋代码

--- level1/p02_is_prime/p02_sol1.cpp
+++ level1/p02_is_prime/p02_sol1.cpp
@@ -9,7 +9,8 @@ int main() {
     int flag = 1;
     cin >> n;
     if(n <= 0) return 0;
-    if(n == 1 || n == 2) flag = 1;
+    if(n == 1) flag = 0;
+    if(n == 2) flag = 1;
     for(int i = 2;i <= n-1;++i) {
         if(n % i == 0) {
             flag = 0;
